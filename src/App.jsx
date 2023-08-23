4881be1e557bc90cf2162cc880f6d7a3c8a4484d
arielm1000 <arielm1000@gmail.com>
2023-08-23 01:19:46 -0300

    reducer01

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
-
+import { useState } from 'react';
 import { AddProduct } from './components/admin/AddProduct';
 import { ListProduct } from './components/admin/ListProduct';
 import { Product } from './components/products/Product';
 import './styles/index.css';
 
+const initialProducts = [
+  {
+    id: 'sku-0001',
+    title: 'Galaxy AS3',
+    category: 'Celulares',
+    price: '$120.000',
+    description: 'Celular 5g con doble pantalla y 4 camaras'
+  },
+  {
+    id: 'sku-0002',
+    title: 'Galaxy A13',
+    category: 'Celulares',
+    price: '$80.000',
+    description: 'Celular 5g con, 3 Camaras'
+  },
+  {
+    id: 'sku-0003',
+    title: 'Galaxy A22 5g',
+    category: 'Celulares',
+    price: '$98.000',
+    description: 'Celular 5g con, 4 Camaras, 27mp'
+  },
+  {
+    id: 'sku-0004',
+    title: 'Galaxy A28 5g',
+    category: 'Celulares',
+    price: '$100.000',
+    description: 'Celular 5g con, 4 Camaras, 27mp'
+  }
+]
 
 function App() {
 
+  const [products, setProducts] = useState(initialProducts)
 
+  const onClickAddProducts = (e, form) => {
+    e.preventDefault();
+    console.log(form);
+}
   return (
     <>
       <div className="container-fliud">
@@ -17,11 +52,11 @@ function App() {
             </div>
         </div>
         <div className="row"  style={{ backgroundColor: '#000', padding:50}}>
-            <AddProduct />
+            <AddProduct onClickAddProducts={(e, value)=>onClickAddProducts(e,value)}/>
             <ListProduct />
         </div>
           <div className='row p-5'>
-            <Product />
+            <Product products={products} />
         </div>
       </div>
     </>
--- src/components/admin/AddProduct.jsx
+++ src/components/admin/AddProduct.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 
-export const AddProduct = () => {
+export const AddProduct = ({onClickAddProducts}) => {
+    
+    const titleRef = useRef(null);
+    const categoryRef = useRef(null);
+    const priceRef = useRef(null);
+    const descriptionRef = useRef(null)
+    const [form, setForm] = useState({});
 
+    const onChangeForm = (value, field) => {
+/*         console.log({
+            ...form,
+            [field]: value,
+        });
+ */        setForm({
+            ...form,
+            [field]: value,
+        })
+
+    }
 
 
   return (
@@ -10,37 +27,46 @@ export const AddProduct = () => {
         <form>
             <div className="mb-3">
                 <input 
+                    ref={titleRef}
                     type="text" 
                     className="form-control"  
                     placeholder="Titulo del  Producto"
-                    name='title' 
+                    name='title'
+                    onChange={(e)=>onChangeForm(e.target.value, titleRef.current.name)}
                 />
             </div>
             <div className="mb-3">
                 <input 
+                    ref={categoryRef}
                     type="text" 
                     className="form-control"  
                     placeholder="Categoria"
                     name='category' 
+                    onChange={(e)=>onChangeForm(e.target.value, categoryRef.current.name)}
                 />
             </div>
             <div className="mb-3">
                 <input 
+                    ref={priceRef}
                     type="text" 
                     className="form-control" 
                     placeholder="Precio"
-                    name='price' 
+                    name='price'
+                    onChange={(e)=>onChangeForm(e.target.value, priceRef.current.name)} 
                 />
             </div>
             <div className="mb-3">
                 <textarea 
+                    ref={descriptionRef}
                     className="form-control" 
                     rows="3" 
                     placeholder='Descripcion del producto'
-                    name='description'>
+                    name='description'
+                    onChange={(e)=>onChangeForm(e.target.value, descriptionRef.current.name)}
+                    >
                 </textarea>
             </div>
-            <button className='btn btn-info'>GUARDAR</button>
+            <button className='btn btn-info' onClick={(e)=>onClickAddProducts(e, form)}>GUARDAR</button>
         </form>
     </div>
   )
